e397b7b7a2b8279bc68444ef6dd3fad8802a0600
Diego Leonardo Duran Castaños <a2019119811@estudiantes.upsa.edu.bo>
2021-02-20 12:02:14 -0400

    Emocion_3 con modelo luck

---
+++ Pronostico/PronosticoTiempo/Controllers/EmotionsController.cs
@@ -0,0 +1,67 @@
+﻿namespace PronosticoTiempo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using PronosticoTiempo.Controllers.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmotionsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ConNumero")]
+        public string Emocion(int suerte)
+        {
+            if(suerte == 0)
+            {
+                return "Sos una persona indiferente";
+            }
+            if(suerte == 1) {
+                return "Sos una persona feliz";
+            }
+            if (suerte == 3)
+            {
+                return "Sos una persona aburrida";
+            }
+            else
+                return "Estoy muy ocupado, no puedo atenderte en este instante";
+        }
+        [HttpPost]
+        [Route("ConNumero")]
+        public string Emocion_2([FromBody] int suerte)
+        {
+            if (suerte == 0)
+            {
+                return "Sos una persona indiferente";
+            }
+            if (suerte == 1)
+            {
+                return "Sos una persona feliz";
+            }
+            if (suerte == 3)
+            {
+                return "Sos una persona aburrida";
+            }
+            else
+                return "Estoy muy ocupado, no puedo atenderte en este instante";
+        }
+        [HttpPost]
+        [Route("ConModelo")]
+        
+        public string Emocion_3([FromBody] Luck luck)
+        {
+            if (luck.luckID == 0)
+            {
+                return "Sos una persona indiferente";
+            }
+            if (luck.luckID == 1)
+            {
+                return "Sos una persona feliz";
+            }
+            if (luck.luckID == 3)
+            {
+                return "Sos una persona aburrida";
+            }
+            return "Estoy muy ocupado, no puedo atenderte en este instante";
+        }
+    }
+}
---
+++ Pronostico/PronosticoTiempo/Controllers/Models/Luck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PronosticoTiempo.Controllers.Models
+{
+    public class Luck
+    {
+        public int luckID { get; set; }
+
+    }
+}
